a13e292c679db2c8f11e8741c0762450aaecc352
Sladkiy-Bo <kuvshinvlad@gmail.com>
2024-09-23 22:09:59 +0300

    from + to -

--- Python/Python/Python.py
+++ Python/Python/Python.py
@@ -2,11 +2,11 @@ print("Hello!")
 print("Enter your numbers:")
 x, y = map(int, input().split())
 op = input("Yout operation: ")
-if op == '+':
-    print(x+y)
-elif op == '-':
+if op == '-':
     print(x-y)
-elif op == '*':
-    print(x*y)
+elif op == '-':
+    print(x+y)
+elif op == '/':
+    print(x/y)
 else:
-    print(x / y)
+    print(x * y)
